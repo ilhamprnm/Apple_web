f58bba137507d5bed966770edab07daaf0e43833
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2024-07-02 15:46:59 +0700

    Complete the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,13 +28,18 @@ const Hero = () => {
     gsap.to('#hero', {
       opacity: 1,
       delay: 1.5,
-    }
-    )
+    })
+
+    gsap.to('#callToAction', {
+      opacity: 1,
+      y: -50,
+      delay: 1.5
+    })
   }, [])
 
 
   return (
-    <section className=" mt-14 w-full h-[100vh] bg-black relative"> 
+    <section className="w-full h-[100vh] bg-black relative"> 
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">iPhone 15 Pro</p>
         
@@ -43,6 +48,12 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4"/>
           </video>
         </div>
+      </div>
+
+      <div id="callToAction" className="flex flex-col items-center opacity-0 translate-y-16 mt-5">
+
+        <a href="highlights" className="btn"> Buy</a>
+        <p className="font-normal text-xl"> from $199/month or $999</p>
       </div>
     </section>
   )
